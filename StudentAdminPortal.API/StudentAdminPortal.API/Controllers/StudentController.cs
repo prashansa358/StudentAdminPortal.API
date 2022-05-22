ac9c8b6224365afadb9344c788389c09a18a9da3
prashansa358 <prashansasinha121@gmail.com>
2022-05-22 17:18:00 +0530

    End Of Section 2 - API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+        public StudentController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var students = await studentRepository.GetStudentsAsync();
+            //with automapper
+            // mapper.Map<List<Student>>(students);
+            //without automapper
+            //var domainModelStudents = new List<Student>();
+            //foreach (var student in students)
+            //{
+            //    domainModelStudents.Add(new Student()
+            //    {
+            //        Id = student.Id,
+            //        FirstName = student.FirstName,
+            //        LastName = student.LastName,
+            //        DateOfBirth = student.DateOfBirth,
+            //        Email = student.Email,
+            //        Mobile = student.Mobile,
+            //        ProfileImageUrl = student.ProfileImageUrl,
+            //        GenderId = student.GenderId,
+            //        Address = new Address()
+            //        { 
+            //            Id = student.Address.Id,
+            //            PhysicalAddress = student.Address.PhysicalAddress,
+            //            PostalAddress = student.Address.PostalAddress,
+            //        },
+            //        Gender = new Gender()
+            //        {
+            //            Id = student.Gender.Id,
+            //            Description = student.Gender.Description,
+            //        }
+            //    });
+            //}
+            //return Ok(domainModelStudents);
+
+            return Ok(mapper.Map<List<Student>>(students));
+        }
+    }
+}
--- StudentAdminPortal.API/StudentAdminPortal.API/Startup.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Repository;
 
 namespace StudentAdminPortal.API
 {
@@ -28,10 +31,15 @@ namespace StudentAdminPortal.API
         {
 
             services.AddControllers();
+            services.AddDbContext<StudentAdminContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("StudentAdminPortalDb")));
+
+            services.AddScoped<IStudentRepository, SqlStudentRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "StudentAdminPortal.API", Version = "v1" });
             });
+            services.AddAutoMapper(typeof(Startup).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
